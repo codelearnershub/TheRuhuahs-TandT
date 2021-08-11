97c4d5a725db39a3a4284bdb5ececf1e27b40c72
oyeleyeoyediran <oyeleyeoyediran20@gmail.com>
2021-08-11 18:45:04 +0100

    initial commit from local repo

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TheRuhuahs_TandT.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
